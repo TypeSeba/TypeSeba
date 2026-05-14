72abac8f1b1397429a0889f046d0b5c892e2e557
Seba <se.bluedesign@gmail.com>
2026-05-13 23:22:23 -0400

    mejoras

--- public/script.js
+++ public/script.js
@@ -42,7 +42,6 @@ try {
         lenis = new Lenis({
             duration: 1.2,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            smooth: true,
         });
 
         function raf(time) {
